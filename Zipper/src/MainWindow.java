2e7e0caa78bdcdb110b4e59208cd81a71f30751c
ZakirC4 <drzakirmoh4@gmail.com>
2024-05-29 16:57:26 +0300

    commit

---
+++ Zipper/src/MainWindow.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow {
+
+    
+
+    public MainWindow() {
+        this.setTitle("Text Editor");
+        this.setPreferredSize(new Dimension(500, 400));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+    }
+}
