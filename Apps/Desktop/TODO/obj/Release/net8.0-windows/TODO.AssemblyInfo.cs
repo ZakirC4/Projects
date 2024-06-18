c6bc772a333ed35490d877e03089f0a85d323fbd
ZakirC4 <drzakirmoh4@gmail.com>
2024-06-18 10:12:28 +0300

    commit

--- Apps/Desktop/TODO/obj/Release/net8.0-windows/TODO.AssemblyInfo.cs
+++ Apps/Desktop/TODO/obj/Release/net8.0-windows/TODO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TODO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a882f57246a4494e90dd26973ced8125d55a59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TODO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TODO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
